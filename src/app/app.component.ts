45161713c3b134dcc503bc188cf99ffc989c121a
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2020-08-16 16:03:44 +0200

    Landing page rewrite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from './services/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
 /*  files = [];
