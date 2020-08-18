c2f3aef3db14615ac7755d7960b2a665f4656ec2
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2020-08-18 22:22:28 +0200

    Files pagination

---
+++ src/app/modules/dashboard-files/components/files-pagination/files-pagination.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-dashboard-files-pagination',
+  templateUrl: './files-pagination.component.html',
+  styleUrls: [ './files-pagination.component.css' ]
+})
+export class FilesPaginationComponent {
+
+  @Input() page: number;
+  @Input() count: number;
+  @Input() total: number;
+
+  @Output() pageChange = new EventEmitter<number>();
+}
