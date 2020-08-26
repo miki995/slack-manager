8ce63591b4322c08d51fa3c209739d8b88344a90
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2020-08-27 01:31:13 +0200

    g analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import { parseQuery } from './helpers/query.helper';
 import * as layoutActions from './store/layout/layout.actions';
 import { Store } from '@ngrx/store';
 import { IAppState } from './store';
+import { NavigationEnd, Router } from '@angular/router';
+import { config } from './config/config';
+
+declare let gtag: any;
 
 @Component({
   selector: 'sc-root',
@@ -14,12 +18,14 @@ export class AppComponent implements OnInit {
 
   constructor(
     private readonly store: Store<IAppState>,
-    private readonly location: Location
+    private readonly location: Location,
+    private readonly router: Router
   ) {
   }
 
   ngOnInit(): void {
     this.handleCode();
+    this.trackGoogleAnalytics();
   }
 
   handleCode(): void {
@@ -34,6 +40,18 @@ export class AppComponent implements OnInit {
     this.store.dispatch({
       type: layoutActions.LAYOUT_EXCHANGE_CODE_FOR_TOKEN,
       payload: code
+    });
+  }
+
+  trackGoogleAnalytics(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', config.ga,
+          {
+            page: event.urlAfterRedirects
+          }
+        );
+      }
     });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   api: 'https://slack-man-api.herokuapp.com',
   redirectUri: 'https://slack-man.web.app/',
+  ga: 'UA-154138019-1'
 };
