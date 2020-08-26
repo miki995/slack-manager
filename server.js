4e183abf2f6a1e006e79de6c30e6a2f1009d2fe1
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2020-08-26 17:28:34 +0200

    Fix heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist/slack-cleaner'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/slack-cleaner/'}),
+);
+
+app.listen(process.env.PORT || 8080);
