86f20eb23a8d7064f3922522051ee14c78a99bea
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2020-08-26 18:21:11 +0200

    Fix heroku angular

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //Install express server
 const express = require('express');
-
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/slack-cleaner'));
 
