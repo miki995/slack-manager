f81d1a866249a8697faed691a9e30674be2a7030
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2020-08-26 18:09:38 +0200

    Fix heroku angular

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+//Install express server
 const express = require('express');
+
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/slack-cleaner'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/slack-cleaner'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/slack-cleaner/index.html'}),
-);
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/slack-cleaner/index.html'));
+});
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
