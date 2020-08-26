fadfb59ba34914f8497e961ac31c4828807e3008
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2020-08-26 16:29:13 +0200

    Fix prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://slackcleaner2.herokuapp.com/',
+  api: 'https://slackcleaner-api.herokuapp.com/',
   redirectUri: 'https://slackcleaner.netlify.app/',
 };
