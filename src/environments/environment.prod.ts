77b653fdd5d561abc9ee5ed8c5aadb3e02177974
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2022-08-27 12:35:59 +0200

    change api to vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://slack-man-api.herokuapp.com',
+  api: 'https://slack-manager-api.vercel.app/',
   redirectUri: 'https://slack-man.web.app/',
   ga: 'UA-154138019-1'
 };
